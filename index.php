be57cc3be73c7b57f56ced480030785a15772d13
rayan <rayanraihani9@gmail.com>
2023-06-16 14:10:31 +0200

    test des fonction qu ne marche pas toute

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
-<?php 
+<?php
 require_once "exo13.php";
-$v1 = new Voiture("Peugot","408",3);
+// echo $voiture2 ->Accelerer(20);
+// // echo $voiture2 ->Demarrer();
+// // echo $voiture2 ->verifieStatut();
+// // echo $voiture2 ->Accelerer(50);
+// // echo $voiture2 ->Arret();
+echo $voiture1 = new Voiture("Peugeot","205",5);
+// echo $voiture2 ->Accelerer(20);
+// // echo $voiture1 ->verifieStatut();
+// echo $voiture1->Accelerer(50);
+// // echo $voiture1->Accelerer(50);
+// echo $voiture1 ->Ralentir(20);
+// echo $voiture1 ->verifieVitesse();
+
+//echo $voiture1 ->setStatut(0);
+//echo $voiture1 ->verifieStatut();
+
+// echo $voiture1 -> limite(20);
+// echo $voiture1 -> verifieVitesse;
 
 
-echo $v1->Start();
-echo $v1->verifieStatut();
-echo $v1->Faster(50);
-echo $v1->Faster(50);
-echo $v1->Ralentir(20);
-echo $v1->verifieVitesse();
 
 
- // echo $v1->verifieSpeed();
-//echo $v1->Arret();
-?>
+  $voiture2= new Voiture("Citroen","c4",5);
+  echo $voiture2 ->Demarrer();
+  echo $voiture2 ->Arret();
+  
+  echo $voiture2 ->verifieStatut();
+  echo $voiture2 ->Accelerer(20);
+  ?>
